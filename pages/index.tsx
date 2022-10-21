85c7fe30742f7f7d9b3ce7d76d001588b070b6ad
Jacob Schroeder <me@jaschr.com>
2022-10-20 21:14:24 -0400

    building up default

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Page = () => {
+	return (
+		<div>
+			<Head>
+				<title>Page Title</title>
+				<meta name="description" content="Page description" />
+			</Head>
+
+			<main>
+				<h1>Index.tsx</h1>
+			</main>
+		</div>
+	)
+}
+
+export default Page
